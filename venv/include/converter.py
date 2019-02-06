f8a5e3f49ecf335686de6d440d6a3406f6117b85
Alex Solonencko <alex.solonencko@gmail.com>
2019-02-06 22:13:33 +0200

    Initialized project with TODOs

---
+++ venv/include/converter.py
@@ -0,0 +1,9 @@
+# TODO: 1. Implement get requests to the API server.
+# TODO: 2. Create database.
+# TODO: 3. Create table in database: CURRENCY, COST
+# TODO: 4. Insert data into database
+# TODO: 5. Make an CLI implementation: list all currencies, preferred currency, convert UAN to SMTH, convert SMTH to UAN
+# TODO: 6. PyQT
+# TODO: 7. TKinter
+# TODO: 8. Kivy
+# TODO: 9. Telegram Bot
