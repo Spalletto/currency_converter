3429b8f9c46a9057046cfe34631ad445908069bb
Alex Solonencko <alex.solonencko@gmail.com>
2019-02-06 22:14:28 +0200

    Added function which collects the data from API

--- venv/include/converter.py
+++ venv/include/converter.py
@@ -7,3 +7,24 @@
 # TODO: 7. TKinter
 # TODO: 8. Kivy
 # TODO: 9. Telegram Bot
+
+import json
+from requests import get
+from pprint import pprint
+
+
+def get_data():
+    url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+
+    response = get(url)
+    data = json.loads(response.content)
+
+    return data
+
+
+def main():
+    get_data()
+
+
+if __name__ == "__main__":
+    main()
